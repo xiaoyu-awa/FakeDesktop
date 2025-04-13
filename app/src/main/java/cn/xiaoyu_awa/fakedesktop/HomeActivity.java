cdf55ab1578a105721c07ae93e44e65848790ba0
wys_yuhao_ <2354934669@qq.com>
2025-04-13 23:20:36 +0800

    优化()

--- app/src/main/java/cn/xiaoyu_awa/fakedesktop/HomeActivity.java
+++ app/src/main/java/cn/xiaoyu_awa/fakedesktop/HomeActivity.java
@@ -11,22 +11,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class HomeActivity extends Activity {
+    TextView date;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         //透明上下两栏
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow(); // in Activity's onCreate() for instance
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         //获取日期文本
-        TextView date = findViewById(R.id.date);
+        date = findViewById(R.id.date);
         Date dNow = new Date();
-        SimpleDateFormat ft = new SimpleDateFormat ("MM月dd日");
-        date.setText(ft.format(dNow));
+        date.setText(new SimpleDateFormat ("MM月dd日").format(dNow));
         //获取日期文本
         TextView week = findViewById(R.id.week);
         week.setText(String.format("%tA%n", dNow));
